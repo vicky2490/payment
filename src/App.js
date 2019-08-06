d72653aa197dadb9248e275c65aea4baf55d32f5
vicky <s353397@sinyi.com.tw>
2019-08-06 17:57:27 +0800

    add wrap, header, progress-bar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Header from './components/Headers/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+    } 
+  }
+  render() {
+    return (
+      <div className="wrap">
+        {<Header/>}
+        <div className="progress-bar">
+          <h1>訂單資訊</h1>
+          <div className="progress">
+              <div className="point point-blue"></div>
+              <div className="line line-gray">
+                <div className="step-word">Step1 選定支付方式</div>
+              </div>
+              <div className="point point-gray"></div>
+              <div className="line line-gray">
+                <div className="step-word">Step2 輸入信用卡資訊或取得支付代碼</div>
+              </div>
+              <div className="point point-gray"></div>
+          </div>
+        </div>
+        <div className="order-content"></div>
+        <div className="payment-content"></div>
+        <div className="precautions"></div>
+        <div className="footer"></div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Headers/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="gray"></div>
+      <div className="orange"></div>
+      <div className="deep-blue"></div>
+      <div className="pink"></div>
+      <div className="red"></div>
+      <div className="blue"></div>
+
+    </div>
+  );
+}
+
+export default Header;
