e8ac94175ef1e9f912da11268f5fb1700460701e
vicky <s353397@sinyi.com.tw>
2019-08-06 16:41:48 +0800

    Updates

---
+++ precache-manifest.d78dd03bf66b7e09905a365cd65e004e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85fc8b57433a3ecf9bc9e567cef442a2",
+    "url": "/payment/index.html"
+  },
+  {
+    "revision": "9b0be25b9538cc09e6b8",
+    "url": "/payment/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/payment/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "9b0be25b9538cc09e6b8",
+    "url": "/payment/static/js/main.e663f702.chunk.js"
+  },
+  {
+    "revision": "8e788c2d187ae1872de2",
+    "url": "/payment/static/js/runtime~main.f0fb4899.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/payment/static/media/logo.5d5d9eef.svg"
+  }
+]);
